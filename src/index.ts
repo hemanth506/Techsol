a489c38d57ccaa2fdad00ee96aa6d546e16e5afe
G.Hemanth Raaj <hemanthraaj31@gmail.com>
2024-01-23 13:04:36 +0530

    removed unwanted imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import mongoose from "mongoose";
 import { DatabaseConnection, ExpressMiddleware } from "./config/default";
 import { envVar } from "./env";
